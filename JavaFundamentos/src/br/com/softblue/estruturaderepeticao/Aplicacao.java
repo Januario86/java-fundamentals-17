e0ff04e1a1a619bf6c02c1c3ee0f2873672efe27
Januario <januario86fp@gmail.com>
2023-01-02 17:54:19 -0300

    while-do-while

---
+++ JavaFundamentos/src/br/com/softblue/estruturaderepeticao/Aplicacao.java
@@ -0,0 +1,30 @@
+package br.com.softblue.estruturaderepeticao;
+
+public class Aplicacao {
+
+	
+	public static void main(String[] args) {
+		
+		int x = 0;
+//		
+//		while(true) {
+//			System.out.println(x);
+//			if(x == 10) {
+//				break;
+//			}
+//			
+//			x++;
+//		}
+		
+//		do {
+//			System.out.println(x);
+//			x++;
+//		}while(x <=10);
+//		
+		
+		
+		
+		
+	}
+	
+}
